d93f2d1068dd525f9b83358e6f78e2008592744e
Gans <73067476+GansGit@users.noreply.github.com>
2023-04-11 17:00:23 +0200

    Update GooseHUB.lua

--- GooseHUB.lua
+++ GooseHUB.lua
@@ -3,7 +3,7 @@ local Window = Library.CreateLib("GooseHUB - Made By Gans", "Midnight")
 local ws = game.Workspace
 local player = game.Players
 if game.PlaceId == 155615604 then
-loadstring(game:httpget("https://raw.githubusercontent.com/GansGit/PrisonLifeScript/main/main.lua"))()
+  loadstring(game:httpget("https://raw.githubusercontent.com/GansGit/PrisonLifeScript/main/main.lua"))()
 end
 
 
