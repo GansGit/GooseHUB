ca7ba0865c1e94be4bbe04a6d90002a924e16b0a
Gans <73067476+GansGit@users.noreply.github.com>
2023-04-11 15:03:35 +0200

    Create GooseHUB.lua

---
+++ GooseHUB.lua
@@ -0,0 +1,9 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("GooseHUB - Made By Gans", "Midnight")
+local ws = game.Workspace
+local player = game.Players
+if game.PlaceId == 155615604 then
+loadstring(game:httpget("https://raw.githubusercontent.com/GansGit/PrisonLifeScript/main/main.lua"))()
+end
+
+
