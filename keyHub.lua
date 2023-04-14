e335443e1d7b0d4b4607fc2d57a8a08bf9e51bfb
Gans <73067476+GansGit@users.noreply.github.com>
2023-04-15 01:43:08 +0200

    Create keyHub.lua

---
+++ keyHub.lua
@@ -0,0 +1,9 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local keyWindow = Library.CreateLib("Key System","DarkTheme")
+local t = keyWindow:NewTab("Key")
+local s = t:NewSection("Key")
+s:NewTextBox("Enter Key:","Enter Key to get in",function(i) 
+if i == "KatsuIsGay" then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GansGit/GooseHUB/main/GooseHUB.lua"))()
+end
+end)
